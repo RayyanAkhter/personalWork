457e33716a3c0b5a239c8fb25fc17f809862e673
Rayyan Akhter <supergokumm@gmail.com>
2022-10-10 12:59:01 +0530

    profile update

--- profile.js
+++ profile.js
@@ -1,11 +1,11 @@
-const button = getById("logoutBtn");
+// const button = getById("logoutBtn");
 
-button.addEventListener("click", logout);
+// button.addEventListener("click", logout);
 
-const spanName = getById("name");
-function initalize() {
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  const { fullName } = currentUser;
-  spanName.innerText = fullName;
-}
-initalize();
+// const spanName = getById("name");
+// function initalize() {
+//   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+//   const { fullName } = currentUser;
+//   spanName.innerText = fullName;
+// }
+// initalize();
